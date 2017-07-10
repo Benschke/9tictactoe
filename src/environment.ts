2803da1c172b930e0c83982b6f47cfed36f87482
Niklas Holtmeyer <niklas.holtmeyer@hs-osnabrueck.de>
2017-07-10 11:47:41 +0200

    enviroment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig  = {
-  apiKey: "AIzaSyDAgqOlxaMGDwx8_0Jh14DEHR_gaN2xNLs",
-  authDomain: "tictactoe-440b3.firebaseapp.com",
-  databaseURL: "https://tictactoe-440b3.firebaseio.com",
-  projectId: "tictactoe-440b3",
-  storageBucket: "tictactoe-440b3.appspot.com",
-  messagingSenderId: "495290538261"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
