b53cbeb188096b2a03a4fecd4eff09231e59ae18
Niklas Holtmeyer <niklas.holtmeyer@hs-osnabrueck.de>
2017-07-10 17:29:54 +0200

    fix

--- platforms/android/build/intermediates/assets/debug/www/build/1.main.js
+++ platforms/android/build/intermediates/assets/debug/www/build/1.main.js
@@ -1,14 +1,14 @@
 webpackJsonp([1],{
 
-/***/ 340:
+/***/ 342:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "GamePageModule", function() { return GamePageModule; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "LobbyPageModule", function() { return LobbyPageModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(36);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__game__ = __webpack_require__(116);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(30);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__lobby__ = __webpack_require__(118);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -18,26 +18,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
-var GamePageModule = (function () {
-    function GamePageModule() {
+var LobbyPageModule = (function () {
+    function LobbyPageModule() {
     }
-    return GamePageModule;
+    return LobbyPageModule;
 }());
-GamePageModule = __decorate([
+LobbyPageModule = __decorate([
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_2__game__["a" /* GamePage */],
+            __WEBPACK_IMPORTED_MODULE_2__lobby__["a" /* LobbyPage */],
         ],
         imports: [
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__game__["a" /* GamePage */]),
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__lobby__["a" /* LobbyPage */]),
         ],
         exports: [
-            __WEBPACK_IMPORTED_MODULE_2__game__["a" /* GamePage */]
+            __WEBPACK_IMPORTED_MODULE_2__lobby__["a" /* LobbyPage */]
         ]
     })
-], GamePageModule);
+], LobbyPageModule);
 
-//# sourceMappingURL=game.module.js.map
+//# sourceMappingURL=lobby.module.js.map
 
 /***/ })
 
