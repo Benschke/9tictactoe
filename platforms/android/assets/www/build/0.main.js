0eb61372c982c870dac36e55f98b2ebe006c2d48
Niklas Holtmeyer <niklas.holtmeyer@hs-osnabrueck.de>
2017-07-14 12:44:57 +0200

    clean up

--- platforms/android/assets/www/build/0.main.js
+++ platforms/android/assets/www/build/0.main.js
@@ -5,10 +5,10 @@ webpackJsonp([0],{
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "TestPageModule", function() { return TestPageModule; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "RulesPageModule", function() { return RulesPageModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(1);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(39);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__test__ = __webpack_require__(294);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__rules__ = __webpack_require__(146);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -18,26 +18,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
-var TestPageModule = (function () {
-    function TestPageModule() {
+var RulesPageModule = (function () {
+    function RulesPageModule() {
     }
-    return TestPageModule;
+    return RulesPageModule;
 }());
-TestPageModule = __decorate([
+RulesPageModule = __decorate([
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_2__test__["a" /* TestPage */],
+            __WEBPACK_IMPORTED_MODULE_2__rules__["a" /* RulesPage */],
         ],
         imports: [
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__test__["a" /* TestPage */]),
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__rules__["a" /* RulesPage */]),
         ],
         exports: [
-            __WEBPACK_IMPORTED_MODULE_2__test__["a" /* TestPage */]
+            __WEBPACK_IMPORTED_MODULE_2__rules__["a" /* RulesPage */]
         ]
     })
-], TestPageModule);
+], RulesPageModule);
 
-//# sourceMappingURL=test.module.js.map
+//# sourceMappingURL=rules.module.js.map
 
 /***/ })
 
